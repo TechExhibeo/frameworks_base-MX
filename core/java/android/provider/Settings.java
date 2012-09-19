e4993fb4ce31fb70f4bd61a1dd41247ed04bfb08
Nicholas Semendyaev <agent00791@gmail.com>
2012-09-18 20:08:36 -0400

    Navigation Bar Custom height [1/2]
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I9ca00129929364bcbd7d6ea11b6d4220543303e4
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/layout/navigation_bar.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3912,6 +3912,12 @@ public final class Settings {
         public static final String RECENTS_SHOW_SEARCH_BAR = "recents_show_search_bar";
 
         /**
+         * Height of navigation bar buttons
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
