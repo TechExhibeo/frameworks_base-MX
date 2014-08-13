7fa57a7b39a688aa45748668e7ffb898b6de21bb
liquid0624 <rjbirnesser@gmail.com>
2014-08-13 19:57:05 -0400

    Frameworks: network traffic color option (1/2)
    Change-Id: I94f28e12cffb46fb36d2b311319bdbd7213d4d7c
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4460,6 +4460,12 @@ public final class Settings {
         public static final String SCREENSHOT_DELAY = "screenshot_delay";
 
         /**
+         * Network stats Color style
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_COLOR = "network_traffic_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
