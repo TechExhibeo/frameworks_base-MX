d9d5a54cec569d653ccf5b0919361c9afae88e6a
Adnan Begovic <adnan@cyngn.com>
2015-03-31 20:16:13 -0700

    SystemUI: Hide secondary icon and divider on expanded panel.
    Change-Id: I9f8a939f9bfcee22e6838dcbbeb6026bcc8146e2

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -867,7 +867,9 @@ public class VolumePanel extends Handler implements DemoMode {
                     mSliderPanel.addView(control.group);
                     control.group.setVisibility(View.VISIBLE);
                     control.expandPanel.setVisibility(View.GONE);
-                    updateSlider(control, true);
+                    control.secondaryIcon.setVisibility(View.GONE);
+                    control.divider.setVisibility(View.GONE);
+                    updateSlider(control, false);
                 }
             }
         }
@@ -882,7 +884,9 @@ public class VolumePanel extends Handler implements DemoMode {
                 if (control != null && control.streamType != mActiveStreamType) {
                     control.group.setVisibility(View.GONE);
                     control.expandPanel.setVisibility(View.GONE);
-                    updateSlider(control, true);
+                    control.secondaryIcon.setVisibility(View.GONE);
+                    control.divider.setVisibility(View.GONE);
+                    updateSlider(control, false);
                 }
             }
         }
