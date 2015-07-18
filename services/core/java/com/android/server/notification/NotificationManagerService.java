463a9fea461ba56859114bbb45c767e2e47acb71
AdrianDC <radian.dc@gmail.com>
2015-07-18 12:20:51 +0200

    Lights with Screen On [1/2]: Optional allowment of lights
    Implement a setting allowing lights to be activated
    for new notifications even if the screen is on.
    
    Lights with screen on and Custom values are separated
    in an advanced section for a cleaner overview.
    
    This setting gives the user an oportunity to activate lights
    with the screen on and also during DayDream screensaver.
    The option is not activated by default.
    
    Changes include :
      frameworks/base
      packages/Apps/Settings
    
    Screenshot of the Settings :
      http://i1285.photobucket.com/albums/a583/adriandc/Screenshot_2015-07-20-23-47-13_zpstpmemwwn.png~original
    
    Change-Id: I2071147d8ddab80ba0e1e7310e785ac3e03b3c7c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -199,6 +200,9 @@ public class NotificationManagerService extends SystemService {
 
     private int mDefaultNotificationLedOff;
     private long[] mDefaultVibrationPattern;
+
+    private boolean mScreenOnEnabled = false;
+    private boolean mScreenOnDefault = false;
 
     private long[] mFallbackVibrationPattern;
     private boolean mUseAttentionLight;
@@ -852,6 +856,9 @@ public class NotificationManagerService extends SystemService {
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD_URI,
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NOTIFICATION_LIGHT_SCREEN_ON),
+                    false, this, UserHandle.USER_ALL);
             update(null);
         }
 
@@ -896,6 +903,11 @@ public class NotificationManagerService extends SystemService {
                        Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0,
                        UserHandle.USER_CURRENT_OR_SELF);
             }
+
+            // Notification lights with screen on
+            mScreenOnEnabled = (Settings.System.getIntForUser(resolver,
+                    Settings.System.NOTIFICATION_LIGHT_SCREEN_ON,
+                    mScreenOnDefault ? 1 : 0, UserHandle.USER_CURRENT) != 0);
 
             updateNotificationPulse();
         }
@@ -2918,7 +2930,7 @@ public class NotificationManagerService extends SystemService {
             enableLed = false;
         } else if (isLedNotificationForcedOn(ledNotification)) {
             enableLed = true;
-        } else if (mInCall || mScreenOn) {
+        } else if (!mScreenOnEnabled && (mInCall || mScreenOn)) {
             enableLed = false;
         } else {
             enableLed = true;
