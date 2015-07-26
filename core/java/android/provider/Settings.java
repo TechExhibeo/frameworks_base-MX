2d4965b6f4214379a8377c345f49afb87b40416f
LorDClockaN <davor@losinj.com>
2015-07-26 14:18:48 +0200

    FWB: Headset icon switch (1/2)
    Change-Id: I561cf99935f9c1ddd0264640dc928bb29d45bdc5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4253,6 +4253,12 @@ public final class Settings {
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
         /**
+         * Switch for headset icon
+         * @hide
+         */
+        public static final String SHOW_HEADSET_ICON = "show_headset_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
